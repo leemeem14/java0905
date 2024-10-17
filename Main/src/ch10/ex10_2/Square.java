4226bbfb6326b23e51170ef7b6e2827c91a5d11b
leemeem14 <leemeem19@naver.com>
2024-10-17 09:54:40 +0900

    1017 JAVA 실습 업캐스팅 범위제한 실습

--- Main/src/ch10/ex10_2/Square.java
+++ Main/src/ch10/ex10_2/Square.java
@@ -2,5 +2,9 @@ package ch10.ex10_2;
 
 
 public class Square extends Shape{
-
+    int pointNum=4;
+    public double area(int width) {
+        double area = Math.pow(width,2);
+        return area;
+    }
 }
